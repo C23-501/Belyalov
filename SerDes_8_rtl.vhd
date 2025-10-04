abd61a27bb377bb45fb21082eb272dfdd309d8f8
val-5 <valerka_2005@mail.ru>
2025-10-04 19:40:34 +0300

    Изменения с 22.09 и 29.09

--- SerDes_8_rtl.vhd
+++ SerDes_8_rtl.vhd
@@ -1,6 +1,7 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 USE ieee.std_logic_unsigned.ALL;
+USE ieee.std_logic_arith.all;
 
 ENTITY SerDes_8 IS
   PORT (
@@ -19,10 +20,13 @@ END ENTITY SerDes_8;
 --
 ARCHITECTURE rtl OF SerDes_8 IS
   --
-  signal SerDataOut_r : std_logic_vector(7 downto 0);
-  signal DesDataOut_r : std_logic_vector(7 downto 0);
-  signal SerCounter   : std_logic_vector(2 downto 0);
-  signal DesCounter   : std_logic_vector(2 downto 0);
+  signal SerEn_r      :  std_logic;
+  signal DesEn_r      :  std_logic;
+  signal SerDataOut_r :  std_logic_vector(7 downto 0);
+  signal DesDataOut_r :  std_logic_vector(7 downto 0);
+  signal DesShift_r   :  std_logic_vector(7 downto 0);
+  signal SerCounter   :  std_logic_vector(2 downto 0);
+  signal DesCounter   :  std_logic_vector(2 downto 0);
   --
 BEGIN
   --
@@ -33,13 +37,20 @@ BEGIN
   begin
     if (nRst = '0') then
       SerDataOut_r <= (others => '0');
+      DesCounter <= conv_std_logic_vector(7, DesCounter'length);
+      SerCounter <= conv_std_logic_vector(7, SerCounter'length);
       DesDataOut_r <= (others => '0');
-      SerCounter <= (others => '0');
-      DesCounter <= (others => '1');
-    elsif (rising_edge(CLK)) then
+      DesShift_r <= (others => '0');
+      DesSTB <= '0';
+     elsif (rising_edge(CLK)) then
+      SerEn_r <= SerEn;
+      DesEn_r <= DesEn;
+      if (DesCounter = conv_std_logic_vector(0, DesCounter'length)) then
+        DesDataOut_r <= DesShift_r;
+      end if;
     --  SerDataOut_r
       if (SerEn = '1') then
-        if (SerCounter = "000") then
+        if (SerCounter = conv_std_logic_vector(0, SerCounter'length) or (SerEn = '1' and SerEn_r = '0')) then
           SerDataOut_r <= SerDataIn;
         else
           for i in 0 to 6 loop
@@ -48,26 +59,34 @@ BEGIN
         end if;
       end if;
     --  SerCounter
-      if (SerEn = '1') then
-        SerCounter <= SerCounter - '1';
+      if (SerEn = '0') then
+        SerCounter <= conv_std_logic_vector(7, SerCounter'length);
       else
-        SerCounter <= (others => '0');
+        if (SerCounter = conv_std_logic_vector(0, SerCounter'length)) then
+          SerCounter <= conv_std_logic_vector(7, SerCounter'length);
+        elsif (SerEn = '1' and SerEn_r = '1') then
+          SerCounter <= SerCounter - '1';
+        end if;
       end if;
-    --  DesDataOut_r
+    --  DesShift_r
       if (DesEn = '1') then
-        DesDataOut_r(0) <= DesDataIn;
+        DesShift_r(0) <= DesDataIn;
         for i in 0 to 6 loop
-          DesDataOut_r(i + 1) <= DesDataOut_r(i);
+          DesShift_r(i + 1) <= DesShift_r(i);
         end loop;
       end if;
     --  DesCounter
       if (DesEn = '0') then
-        DesCounter <= (others => '1');
+        DesCounter <= conv_std_logic_vector(7, DesCounter'length);
       else
-        DesCounter <= DesCounter - '1';
+        if (DesCounter = conv_std_logic_vector(0, DesCounter'length)) then
+          DesCounter <= conv_std_logic_vector(7, DesCounter'length);
+        elsif (DesEn = '1' and DesEn_r = '1') then
+          DesCounter <= DesCounter - '1';
+        end if;
       end if;
     --  DesSTB
-      if (DesCounter = "000") then
+      if (DesCounter = conv_std_logic_vector(0, DesCounter'length)) then
         DesSTB <= '1';
       else
         DesSTB <= '0';
